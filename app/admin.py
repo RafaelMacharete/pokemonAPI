d365e0c3fd1daab8763a6287414b323facf76769
Thalia <pereirathalia806@gmail.com>
2025-01-10 11:08:27 -0300

    10.01.2025 11:00 - creating new data

--- app/admin.py
+++ app/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 # Register your models here.
 
+# Pokemon Info
 class datPokemon(admin.ModelAdmin):
     list_display = ('id', 'name')
     list_display_links = ('id',)
@@ -10,3 +11,50 @@ class datPokemon(admin.ModelAdmin):
     list_per_page = 10
     
 admin.site.register(Pokemon, datPokemon)
+
+# Ginasy Info
+class datGinasy(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id',)
+    search_fields = ('name',)
+    list_per_page = 10
+    
+admin.site.register(Ginasy, datGinasy)
+
+# GameGeneration Info
+class datGameGeneration(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('id',)
+    search_fields = ('name',)
+    list_per_page = 10
+    
+admin.site.register(GameGeneration, datGameGeneration)
+
+# Game Pokedex Info
+
+# class datGamePokedex(models.Model):
+#     list_display = ('id', 'name')
+#     list_display_links = ('id',)
+#     search_fields = ('name',)
+#     list_per_page = 10
+
+# admin.site.register(GamePokedex, datGamePokedex)
+
+# # Pokemon Entry Info
+
+# class datPokemonEntry(models.Model):
+#     list_display = ('id', 'name')
+#     list_display_links = ('id',)
+#     search_fields = ('name',)
+#     list_per_page = 10
+
+
+# # Pokemon Register Info
+
+# class datPokemonRegister(models.Model):
+#     list_display = ('id', 'name')
+#     list_display_links = ('id',)
+#     search_fields = ('name',)
+#     list_per_page = 10
+
+# admin.site.register(PokemonRegister, datPokemonRegister)
--- app/models.py
+++ app/models.py
@@ -6,9 +6,62 @@ from django.db import models
 class Pokemon(models.Model):
     name = models.CharField(max_length=100) # Campo de texto com limitação
     url = models.TextField() # Campo de texto sem limitação
+
+    def _str_(self):
+        return self.name
     
-class Game(models.Model):
+    
+class Ginasy(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    
+    def _str_(self):
+        return self.name
+
+class GameGeneration(models.Model):
     name = models.CharField(max_length=100)
-    abilities = models.TextField(max_length=30)
-    names = models.TextField(max_length=40)
-    main_region = models.TextField(max_length=30)
+    
+    ginasy = models.ForeignKey(Ginasy, verbose_name="Pokemon's Ginasy", on_delete=models.CASCADE)
+    # Agora o jogo, depende de um registro de um personagem para ser criado
+    ability = models.CharField(max_length=25)
+    name = models.CharField(max_length=30)
+    main_region = models.CharField(max_length=20)
+    moves = models.CharField(max_length=25)
+    pokemon_species = models.CharField(max_length=25)
+    types = models.CharField(max_length=25)
+    version_groups = models.CharField(max_length=50)
+    created = models.DateField(editable=False, auto_now=True)
+    
+    def _str_(self):
+        return self.name
+    
+# class GamePokedex(models.Model):
+#     name = models.CharField(max_length=25)
+#     pokedex = models.ForeignKey(GameGeneration, verbose_name='Pokedex Game', on_delete=models.CASCADE)
+
+#     is_main_series = models.BooleanField()
+#     descriptions = models.TextField()
+#     name_in_another_language = models.CharField(max_length=25)
+#     region = models.CharField(max_length=25)
+#     version_groups = models.CharField(25)
+    
+#     created = models.DateField(editable=False, auto_now=True)
+    
+#     def _str_(self):
+#         return self.name
+
+# class PokemonRegister(models.Model):
+#     name = models.CharField(max_length=30)
+#     is_main_series = models.BooleanField()
+
+#     pokedex = models.ForeignKey(GameGeneration, verbose_name='Pokedex Game', on_delete=models.CASCADE)
+
+#     generation = models.CharField(max_length=30)
+#     name_in_another_language = models.CharField(max_length=30)
+#     pokemon_type = models.CharField(max_length=30)
+    
+#     created = models.DateField(editable=False, auto_now=True)
+    
+#     def _str_(self):
+#         return self.name
+    
